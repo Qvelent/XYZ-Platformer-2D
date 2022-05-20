fb578cac6e14707660a167700891f2ce897a766d
Qvelent <qvelent@gmail.com>
2022-05-20 17:59:14 +0300

    add mainmenu

---
+++ XYZ 2D Platformer/Assets/PlayerOption/Scripts/UI/Widgets/CustomButtom.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using Button = UnityEngine.UI.Button;
+
+namespace PlayerOption.Scripts.UI.Widgets
+{
+    public class CustomButtom : Button
+    {
+        [SerializeField] private GameObject _normal;
+        [SerializeField] private GameObject _pressed;
+
+        protected override void DoStateTransition(SelectionState state,bool instant)
+        {
+            base.DoStateTransition(state, instant);
+            
+            _normal.SetActive(state != SelectionState.Pressed);
+            _pressed.SetActive(state == SelectionState.Pressed);
+            
+        }
+    }
+}
