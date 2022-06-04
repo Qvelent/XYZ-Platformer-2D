cdabef523445869f4c6f83c675cbd62ec0823853
Qvelent <qvelent@gmail.com>
2022-06-04 11:49:51 +0300

    add audio setting component

---
+++ XYZ 2D Platformer/Assets/PlayerOption/Scripts/Components/Audio/AudioSettingComponent.cs
@@ -0,0 +1,41 @@
+﻿using Assets.PlayerOption.Scripts.Model.Data;
+using Assets.PlayerOption.Scripts.Model.Data.Properties;
+using System;
+using UnityEngine;
+
+namespace Assets.PlayerOption.Scripts.Components.Audio
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class AudioSettingComponent : MonoBehaviour
+    {
+        [SerializeField] private SoundEffect _mode;
+        private AudioSource _source;
+        private FloatPersistentProperty _model;
+
+        private void Start()
+        {
+            _source = GetComponent<AudioSource>();
+            _model = FindProperty();
+            _model.OnChanged += OnSoundSettingChanged;
+            OnSoundSettingChanged(_model.Value, _model.Value);
+        }
+
+        private void OnSoundSettingChanged(float newValue, float oldValue)
+        {
+            _source.volume = newValue;
+        }
+
+        private FloatPersistentProperty FindProperty()
+        {
+            switch (_mode)
+            {
+                case SoundEffect.Music:
+                    return GameSettings.I.Music;
+                case SoundEffect.Sfx:
+                    return GameSettings.I.Sfx;
+            }
+
+            throw new ArgumentException("Undefined mode");
+        }
+    }
+}
